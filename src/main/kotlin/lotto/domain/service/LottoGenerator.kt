d9e0ccad1db5cbc1b0c64dbd72b8bbea1ff15d80
양정우 <jw.yang@hyundai-autoever.com>
2026-05-06 00:42:59 +0900

    feat: Lottos 일급 컬렉션 및 대량 생성 기능 구현

--- src/main/kotlin/lotto/domain/service/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/service/LottoGenerator.kt
@@ -1,10 +1,18 @@
 package lotto.domain.service
 
 import lotto.domain.Lotto
+import lotto.domain.Lottos
+import lotto.domain.PurchaseAmount
 
 class LottoGenerator {
     fun generate(): Lotto {
         val numbers = (1..45).shuffled().take(6)
         return Lotto(numbers)
+    }
+
+    fun generateLottos(purchaseAmount: PurchaseAmount): Lottos {
+        val count = purchaseAmount.getLottoPurchaseCount()
+        val lottos = List(count) { generate() }
+        return Lottos(lottos)
     }
 }
--- src/test/kotlin/lotto/domain/service/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/service/LottoGeneratorTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain.service
 
+import lotto.domain.PurchaseAmount
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -54,5 +55,48 @@ class LottoGeneratorTest {
         assertThat(lotto1.numbers).hasSize(6)
         assertThat(lotto2.numbers).hasSize(6)
         assertThat(lotto3.numbers).hasSize(6)
+    }
+
+    @Test
+    fun `구매 금액에 따라 로또를 생성한다`() {
+        // given
+        val generator = LottoGenerator()
+        val purchaseAmount = PurchaseAmount(5000)
+
+        // when
+        val lottos = generator.generateLottos(purchaseAmount)
+
+        // then
+        assertThat(lottos.lottos).hasSize(5)
+    }
+
+    @Test
+    fun `생성된 모든 로또는 유효한 번호를 가진다`() {
+        // given
+        val generator = LottoGenerator()
+        val purchaseAmount = PurchaseAmount(3000)
+
+        // when
+        val lottos = generator.generateLottos(purchaseAmount)
+
+        // then
+        lottos.lottos.forEach { lotto ->
+            assertThat(lotto.numbers).hasSize(6)
+            assertThat(lotto.numbers).allMatch { it in 1..45 }
+            assertThat(lotto.numbers).doesNotHaveDuplicates()
+        }
+    }
+
+    @Test
+    fun `1000원으로 1개의 로또를 생성한다`() {
+        // given
+        val generator = LottoGenerator()
+        val purchaseAmount = PurchaseAmount(1000)
+
+        // when
+        val lottos = generator.generateLottos(purchaseAmount)
+
+        // then
+        assertThat(lottos.lottos).hasSize(1)
     }
 }
